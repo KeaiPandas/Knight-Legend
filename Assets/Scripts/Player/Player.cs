77096875d3f3bbc280f337dc1e5978066ac90c13
KeaiPandas <318455285@qq.com>
2024-06-24 22:40:58 +0800

    blackhole

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -37,6 +37,7 @@ public class Player : Entity
     public PlayerCounterAttackState counterAttack { get; private set; }
     public PlayerAimSwordState aimSword { get; private set; }
     public PlayerCatchSwordState catchSword { get; private set; }
+    public PlayerBlackholeState blackHole { get; private set; }
     #endregion
 
     protected override void Awake()
@@ -57,7 +58,8 @@ public class Player : Entity
 
         aimSword = new PlayerAimSwordState(this, stateMachine, "AimSword");
         catchSword = new PlayerCatchSwordState(this, stateMachine, "CatchSword");
-
+        catchSword = new PlayerCatchSwordState(this, stateMachine, "CatchSword");
+        blackHole = new PlayerBlackholeState(this, stateMachine, "Jump");
     }
 
     protected override void Start()
---
+++ Assets/Scripts/Player/PlayerBlackholeState.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBlackholeState : PlayerState
+{
+    private float flyTime = .4f;
+    private bool skillUsed;
+    public PlayerBlackholeState(Player _player, PlayerStateMachine _stateMachine, string _animBoolName) : base(_player, _stateMachine, _animBoolName)
+    {
+    }
+
+    public override void AnimationFinishTrigged()
+    {
+        base.AnimationFinishTrigged();
+    }
+
+    public override void Enter()
+    {
+        base.Enter();
+        skillUsed = false;
+        stateTimer = flyTime;
+        rb.gravityScale = 0;
+    }
+
+    public override void Exit()
+    {
+        base.Exit();
+    }
+
+    public override void Update()
+    {
+        base.Update();
+
+        if (stateTimer > 0)
+            rb.velocity = new Vector2(0, 15);
+
+        if (stateTimer < 0)
+        {
+            rb.velocity = new Vector2(0, -.1f);
+            if (!skillUsed)
+            {
+                Debug.Log("CAST BLACKHOLE");
+                skillUsed = true;
+            }
+
+        }
+    }
+}
